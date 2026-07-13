f3a45126386d6ac3a60f7813b0f6d940f0b3d1ad
Dr. K.D. Murray <foss@kdmurray.id.au>
2026-07-13 13:05:12 +1000

    read_delims: Document ...

--- R/read_delims.R
+++ R/read_delims.R
@@ -1,6 +1,7 @@
 #' readr::read_delim() for many files at once
 #'
 #' @param files  A character vector of filenames
+#' @param ...    Arguments passed to each file-level readr::read_delim() call
 #' @return       The resulting concatenated dataframe
 #' @export
 read_delims = function(files, ...) {
